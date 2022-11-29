d8a47d68df334debee5f7b9f0b05faa9a56e7777
NiksaKepecc <98954446+NiksaKepecc@users.noreply.github.com>
2022-11-30 00:04:10 +0100

    cameraFollowUsavrsen

--- My project/Assets/scripts/cameraFolow.cs
+++ My project/Assets/scripts/cameraFolow.cs
@@ -5,54 +5,58 @@ using UnityEngine;
 public class cameraFolow : MonoBehaviour
 {
     public Transform player;
+    public Transform EndBox;
+    
     public float smoothSpeed = 0.125f;
-    public Vector3 offset;
     public float EndX;
     public float EndZ;
+    public float EndBoxX;
+    public float EndBoxZ;
+    
+    public Vector3 offset;
     public Vector3 END;
+    public Vector3 box;
+
     private void FixedUpdate()
     {
-        END = new Vector3(EndX, 0, EndZ);
+       
+        
+
         Vector3 desiredPosition = player.position + offset;
         Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothPosition + END;
+        
 
+        box = EndBox.localScale;
+        EndBoxX = box.x;
+        EndBoxZ = box.z;
         
         Vector3 end = transform.position;
-        if(end.x > 20)
+        if(end.x > EndBoxX)
         {
-            EndX = 20 - end.x;
+            EndX = EndBoxX - end.x;
+        }
+        else if(end.x < -EndBoxX)
+        {
+            EndX = -EndBoxX - end.x ;
         }
         else
         {
             EndX = 0;
         }
 
-        if (end.z > 20)
+        if (end.z > EndBoxZ)
         {
-            EndZ = 20 - end.z;
+            EndZ = EndBoxZ - end.z;
+        }
+        else if(end.z < -EndBoxZ)
+        {
+            EndZ = -EndBoxZ - end.z;
         }
         else
         {
             EndZ = 0;
         }
-
-        if (end.x > 20)
-        {
-            EndX = 20 - end.x;
-        }
-        else
-        {
-            EndX = 0;
-        }
-
-        if (end.z > 20)
-        {
-            EndZ = 20 - end.z;
-        }
-        else
-        {
-            EndZ = 0;
-        }
+        END = new Vector3(EndX, 0, EndZ);
     }
 }
