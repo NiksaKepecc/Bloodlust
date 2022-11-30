a6d15da749346858d49b78abde702119cc50cf49
NiksaKepecc <98954446+NiksaKepecc@users.noreply.github.com>
2022-11-30 11:54:34 +0100

    b

--- My project/Assets/scripts/PlayerMovementt.cs
+++ My project/Assets/scripts/PlayerMovementt.cs
@@ -4,29 +4,41 @@ using UnityEngine;
 
 public class PlayerMovementt : MonoBehaviour
 {
-    public CharacterController controller;
-    public Vector3 playerVelocity;
-    public bool groundedPlayer;
-    public float playerSpeed = 2.0f;
-    public float jumpHeight = 1.0f;
-    public float gravityValue = -9.81f;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 10.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
 
     private void Start()
     {
-        
+        controller = gameObject.AddComponent<CharacterController>();
     }
 
     void Update()
     {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
 
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * playerSpeed);
 
-        if (move != Vector3.zero) 
+        if (move != Vector3.zero)
         {
             gameObject.transform.forward = move;
         }
 
-        
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
